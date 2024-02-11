417109f29533fdbd213a756da6b649cdcae04fc6
atifraza158 <officialatifraza@gmail.com>
2024-02-11 15:13:42 +0500

    Add Validation to check the link input is not empty

--- youtube/views.py
+++ youtube/views.py
@@ -1,18 +1,22 @@
 
 from django.shortcuts import render, redirect
 from pytube import *
+from django.contrib import messages
 
 def youtube_downloader(request):
     if request.method == 'POST':
         link = request.POST.get('link')
         
-        video = YouTube(link)
-        stream = video.streams.get_lowest_resolution()
-        # stream = video.streams.filter(res="720p").first()
-        
-        # stream.download()
-        # print('Video downloaded successfully')
-        # return render(request, 'home.html')
-        download_url = stream.url
-        return redirect(download_url)
+        if link is not None:
+            video = YouTube(link)
+            stream = video.streams.get_lowest_resolution()
+            # stream = video.streams.filter(res="720p").first()
+            
+            # stream.download()
+            # print('Video downloaded successfully')
+            # return render(request, 'home.html')
+            download_url = stream.url
+            return redirect(download_url)
+        else :
+            messages.error(request, "Must Pass the link")
     return render(request, 'home.html')
